8c879433a3dc48e8f7e21309ddf3d85b49c4fd30
Dmytro Lukianchikov <dimon.xr@gmail.com>
2020-08-24 14:30:01 +0300

    test 2

--- dags/tutorial.py
+++ dags/tutorial.py
@@ -12,7 +12,7 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
+    'queue': 'aws',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
